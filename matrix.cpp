09bfd28180ecb0da27b24b4610d93df36e54c97a
Jacob Hicks <jhicks70@ut.utm.edu>
2026-02-26 03:21:03 +0000

    Matrix Operations MKVII

--- matrix.cpp
+++ matrix.cpp
@@ -242,9 +242,9 @@ std::ostream& operator<<(std::ostream& os, const Matrix& m)
 {
     for (int i = 0; i < m.getRows(); ++i) {
         for (int j = 0; j < m.getCols(); ++j) {
-            os << std::setw(10) << m.at(i, j);
+            os << std::setw(10) << m.at(i, j) << " ";
         }
-        os << " \n";
+        os << std::endl;
     }
     return os;
 }
